bf9708d1939e13fb63e54ac73d5a0e7e1611a7eb
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2015-01-26 11:54:42 -0300

    update

--- Website/Judah/View/Frames/Common/Common/Window/Common/Code/Common.php
+++ Website/Judah/View/Frames/Common/Common/Window/Common/Code/Common.php
@@ -1,28 +1,8 @@
 <div id="DivIdCode">
-    <div id="DivIdCodeBackground">
-        <div id="DivIdText">
-            <?php
-            for($indexBackground=0;$indexBackground<100;$indexBackground++) {
-            ?>
-                01001010 01110101 01100100 01100001 01101000 00100000 01001000 01101111 01101100 01100001 01101110 01100100 01100001 00100000 01000011 01101111 01110010 01110010 01100101 01101001 01100001 00100000 01001100 01101001 01101101 01100001
-            <?php
-            }
-            ?>
-        </div>
-    </div>
-
     <div id="DivIdProjectTitle">
         <div id="DivIdText">
-            <div id="DivIdProjectTitleText" contenteditable="true">
-                Project Title
-            </div>
-        </div>
-    </div>
-
-    <div id="DivIdCodeTitle">
-        <div id="DivIdText">
-            <div id="DivIdCodeTitleText" contenteditable="true">
-                Code Title
+            <div id="DivIdProjectTitleText">
+                JUDAH
             </div>
         </div>
     </div>
